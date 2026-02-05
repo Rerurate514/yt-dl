74c9890ab5da277ae950a78884989f6a11030c10
Rerurate514 <rerurateyuto@gmail.com>
2026-02-06 00:34:39 +0900

    UIの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:yt_dl/ytDl.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final TextEditingController _controller = TextEditingController();
+
+  Future<void> _download() async {
+    final ytdl = YtDownloader();
+
+    final isSucceed = await ytdl.downloadMp3FromYt(_controller.text);
+    debugPrint(isSucceed.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Padding(
+          padding: const EdgeInsets.all(128),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              TextButton(
+                onPressed: _download, 
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.download),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text("Download")
+                  ],
+                )
+              )
+            ],
+          ),
+        )
       ),
     );
   }
